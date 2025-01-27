7b71dee25571055817c33a312b01d25688c293c4
kazuaki <kazuaki@archlinux.localdomain>
2025-01-27 18:35:50 +0530

    ktor work with jason format

--- jasonInKtor/src/main/kotlin/Routing.kt
+++ jasonInKtor/src/main/kotlin/Routing.kt
@@ -1,16 +1,29 @@
 package com.Handling
 
+import com.Handling.model.User
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        post("/user") {
+            val user = call.receive<User>()
+            call.respondText("Hello, ${user.name}, you are ${user.age} years old!")
+        }
+        get("/users"){
+            val user = call.respond(
+                listOf(
+                    User("Nishant",20),
+                    User("Rahul",30),
+                    User("Jadu",40),
+                    User("Rohit",50)
+                )
+            )
         }
     }
 }
---
+++ jasonInKtor/src/main/kotlin/model/User.kt
@@ -0,0 +1,9 @@
+package com.Handling.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val name : String,
+    val age : Int
+)
